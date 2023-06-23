2d793db4f94797a2909e7cc6ea9b05d10a7aa98e
DanRuizK57 <d.ruiz03@ufromail.cl>
2023-06-23 16:38:02 -0400

    Registro de usuario añadido

---
+++ laligapremier-api/src/controllers/usuario.controller.js
@@ -0,0 +1,42 @@
+import UsuarioModel from '../models/usuario.model.js';
+import bcrypt from 'bcrypt';
+
+async function registrarUsuario(req, res) {
+    try {
+      const nombre = req.body.nombre;
+      const correo = req.body.correo;
+      const rol = req.body.rol;
+  
+      let clave = req.body.clave;
+      const claveEncriptada = bcrypt.hashSync(clave, 10);
+      clave = claveEncriptada;
+  
+      let error = "Falta el campo ";
+  
+      if(nombre === undefined){
+          error += "nombre"
+          res.status(400).send({error: error});
+      } else if (correo === undefined){
+          error += "correo"
+          res.status(400).send({error: error});
+      } else if (clave === undefined) {
+          error += "clave"
+          res.status(400).send({error: error});
+      } else {
+
+        if (rol != undefined){
+            await UsuarioModel.create({nombre: nombre, correo: correo, rol: rol, clave: clave});
+            res.send(true);
+          } else {
+            await UsuarioModel.create({nombre: nombre, correo: correo, rol: "usuario", clave: clave});
+            res.send(true);
+          }  
+
+      }
+    
+    } catch (err) {
+      res.status(500).send({error: err});
+    }
+}
+
+export { registrarUsuario };
--- laligapremier-api/src/index.js
+++ laligapremier-api/src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import envs from './configs/environments.js';
 //import ruta from './routes/mensajeria.routes.js';
+import authRoute from './routes/auth.routes.js';
 import connect from './configs/mongo.js';
 
 const app = express();
@@ -10,6 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 //app.use('', ruta);
+app.use('/auth', authRoute);
 
 console.log('Conectando a la base de datos...');
 connect()
