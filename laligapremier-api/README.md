a3acc303c7ba7d855a2faf501ee237852fae91d9
DanRuizK57 <d.ruiz03@ufromail.cl>
2023-06-23 15:38:30 -0400

    Base de la API añadida

---
+++ laligapremier-api/README.md
@@ -0,0 +1,9 @@
+# API de LaLiga Premier
+Desarrollo de la API de la aplicación.
+
+# Iniciar aplicación
+npm run start
+
+# Variables de entorno
+PORT= Puerto en el que se inicia la aplicación
+MONGO_URI= Dirección de la base de datos
---
+++ laligapremier-api/src/index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import cors from 'cors';
+import envs from './configs/environments.js';
+//import ruta from './routes/mensajeria.routes.js';
+import connect from './configs/mongo.js';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+//app.use('', ruta);
+
+console.log('Conectando a la base de datos...');
+connect()
+  .then(() => {
+    console.log('MongoDB Conectado Correctamente');
+    app.listen(envs.PORT, async () => {
+      console.log(`Servidor iniciado en el PUERTO: ${envs.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+    process.exit(-1);
+  });
