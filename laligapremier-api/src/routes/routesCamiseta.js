0ac475beda696a458387efdf267aac0c568d2651
GabeASM <g.aillapan01@ufromail.cl>
2023-07-04 12:51:12 -0400

    autenticacion en la aplicacion

--- laligapremier-api/src/routes/routesCamiseta.js
+++ laligapremier-api/src/routes/routesCamiseta.js
@@ -14,7 +14,8 @@ router.get('/camisetas' , listarCamiseta)
 
 router.post('/crear-camiseta' , authRequired , hasRole("administrador") ,crearCamiseta),
 
-router.put('/editarcamiseta/:idCamiseta', editarCamiseta)
+router.put('/editarcamiseta/:idCamiseta', authRequired , hasRole("administrador"), editarCamiseta)
 
-router.delete('/eliminar-camiseta/:idCamiseta' , eliminarCamiseta)
+router.delete('/eliminar-camiseta/:idCamiseta' , authRequired , hasRole("administrador"), eliminarCamiseta)
+
 export default router;
